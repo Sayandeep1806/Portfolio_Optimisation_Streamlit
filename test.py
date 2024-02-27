331aef80ec26557cda84bfeef2edc50e73683ad3
Sayandeep Ghosh <49628452+Sayandeep1806@users.noreply.github.com>
2024-02-27 09:16:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 from datetime import datetime, timedelta
+from statsmodels.tsa.arima.model import ARIMA
+import matplotlib.pyplot as plt
 
 # Sample data provided
 data = {
