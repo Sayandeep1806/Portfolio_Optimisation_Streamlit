f989fd9303f2342ccec5a0f59ab783aa1c631111
Sayandeep Ghosh <49628452+Sayandeep1806@users.noreply.github.com>
2024-02-29 10:58:06 +0530

    Update test.py

--- test.py
+++ test.py
@@ -67,6 +67,7 @@ def forecast_SPX(spx_data, in_sample_start_month, out_sample_end_month):
 st.title('Portfolio Optimisation Tool')
 
 # Ask the user to enter the risk aversion
+st.write("## Select The User Risk Aversion")
 risk_aversion = st.slider('Risk Aversion (γ)', min_value=0.1, max_value=10.0, step=0.1, value=2.0)
 
 # Find min and max months
